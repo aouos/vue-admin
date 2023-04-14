a9ae7f145a6b13f7a8e8bc5c5a1afa673a1b2818
aouos <aouos@qq.com>
2023-04-14 21:47:14 +0800

    feat: use pinia status management

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import useCounterStore from '@/store/counter';
+
+const counterStore = useCounterStore();
+
+const changeCounter = () => {
+  counterStore.changeCounterAction(counterStore.counter + 1);
+};
+</script>
 
 <template>
-  <div class="main">main</div>
+  <div class="main">
+    {{ counterStore.counter }}
+  </div>
+  <p>{{ counterStore.doubleCounter }}</p>
+  <button @click="changeCounter">+1</button>
 </template>
 
 <style scoped>
