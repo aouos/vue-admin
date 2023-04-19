8cf876886ee67719ac29f2734ad4bf55e65ed781
aouos <aouos@qq.com>
2023-04-19 21:56:19 +0800

    feat: main page layout

---
+++ src/components/xmenu/xmenu.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="xmenu">
+    <div class="logo">后台管理系统</div>
+    <div class="menu"></div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.xmenu {
+  user-select: none;
+}
+
+.logo {
+  width: 100%;
+  text-align: center;
+  font-size: 24px;
+  line-height: 48px;
+  color: #fff;
+}
+</style>
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,35 +1,35 @@
 <script lang="ts" setup>
-import useCounterStore from '@/store/counter';
-import { baseRequest } from '@/service/index';
-import { ElMessage } from 'element-plus';
-
-const counterStore = useCounterStore();
-
-const changeCounter = () => {
-  counterStore.changeCounterAction(counterStore.counter + 1);
-};
-
-const requestData = () => {
-  baseRequest.get({ url: '/login' }).then((res) => {
-    console.log(res);
-
-    ElMessage.success('请求成功');
-  });
-};
+import Xmenu from '@/components/xmenu/xmenu.vue';
 </script>
 
 <template>
-  <div class="main">
-    {{ counterStore.counter }}
-  </div>
-  <p>{{ counterStore.doubleCounter }}</p>
-  <el-button type="primary" @click="changeCounter">count +1</el-button>
-
-  <el-button type="danger" @click="requestData">请求数据</el-button>
+  <el-container class="layout">
+    <el-aside class="layout-aside">
+      <xmenu />
+    </el-aside>
+    <el-container>
+      <el-header class="layout-header">Header</el-header>
+      <el-main class="layout-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
-<style scoped>
-.main {
-  color: blueviolet;
+<style lang="less" scoped>
+.layout {
+  height: 100%;
+  width: 100%;
+
+  &-aside {
+    width: 200px;
+    background-color: #7563f6;
+  }
+
+  &-header {
+    background-color: blanchedalmond;
+  }
+
+  &-main {
+    background-color: aliceblue;
+  }
 }
 </style>
